ad311d8637282b8cb035533cea4f14f81f9a6e11
Kim Jeong-won <threedalpeng@gmail.com>
2020-08-24 02:19:05 +0900

    Modify Realtime Router

--- backend/routes/realtime/realtime.js
+++ backend/routes/realtime/realtime.js
@@ -1,17 +1,17 @@
-const recieveStream = (io) => {
+function recieveStream(io) {
     io.on('connection', (socket) => {
         socket.on('camstream', processStream(stream));
     })
 }
 
-const processStream = (stream) => {
+function processStream(stream) {
     stream.total_people = stream.mask_weared + stream.mask_off + stream.mask_incorrect + stream.mask_unknown;
     stream.mask_off = stream.mask_off + stream.mask_incorrect;
+    stream.img = stream.img.toString("base64");
     sendStream(stream);
 }
 
-const sendStream = (stream) => {
-    console.log(stream);
+function sendStream(stream) {
     io.broadcast.emit('stream_display', stream);
 }
 
