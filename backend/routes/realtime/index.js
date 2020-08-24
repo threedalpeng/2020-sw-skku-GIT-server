7014e381eec415725eb7057d16fdb2b377736974
Kim Jeong-won <threedalpeng@gmail.com>
2020-08-25 00:59:43 +0900

    Modify a Realtime Router

--- backend/routes/realtime/index.js
+++ backend/routes/realtime/index.js
@@ -2,10 +2,19 @@ const express = require('express')
 const router = express.Router();
 
 const {
-    recieveStream
+    processStream
 } = require('./realtime')
 
+function streamAnalyzedImage(io) {
+    io.on('connection', (socket) => {
+        socket.on('camstream', (stream) => {
+            let processedStream = processStream(stream);
+            io.broadcast.emit('stream_display', processedStream);
+        });
+    });
+}
+
 module.exports = (io) => {
-    recieveStream(io);
+    streamAnalyzedImage(io);
     return router;
 }
--- backend/routes/realtime/realtime.js
+++ backend/routes/realtime/realtime.js
@@ -1,20 +1,10 @@
-function recieveStream(io) {
-    io.on('connection', (socket) => {
-        socket.on('camstream', processStream(stream));
-    })
-}
-
 function processStream(stream) {
     stream.total_people = stream.mask_weared + stream.mask_off + stream.mask_incorrect + stream.mask_unknown;
     stream.mask_off = stream.mask_off + stream.mask_incorrect;
     stream.img = stream.img.toString("base64");
-    sendStream(stream);
-}
-
-function sendStream(stream) {
-    io.broadcast.emit('stream_display', stream);
+    return stream;
 }
 
 module.exports = {
-    recieveStream
+    processStream
 };
