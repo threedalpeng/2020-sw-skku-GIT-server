16063d89da28a0ada9c44932d72d4b8e4c177584
Kim Jeong-won <threedalpeng@gmail.com>
2020-08-28 23:01:18 +0900

    [UPDATE] Update UI_02.vue

--- frontend/src/views/UI_02.vue
+++ frontend/src/views/UI_02.vue
@@ -2,36 +2,17 @@
   <div class="UI_02">
     <h1>Second UI</h1>
     <v-container fluid>
-      <v-row
-        :align='center'
-        :justify='center'
-        class="grey lighten-5"
-        style="height: 400px;">
-        <v-col
-          cols="12"
-            md="2"
-        >
-          <v-date-picker
-            v-model="picker"
-            width="200"
-            :landscape="landscape"
-            :reactive="reactive"
-            :flat="flat"
-            :show-current="showCurrent"
-            :type="month ? 'month' : 'date'"
-            :multiple="multiple"
-            :readonly="readonly"
-            :disabled="disabled"
-            :events="enableEvents ? functionEvents : null"
-          ></v-date-picker>
+      <v-row>
+        <v-col cols="12" sm="2" class="my-2 px-1">
+          <v-date-picker v-model="picker" @click='getData()' ></v-date-picker>
         </v-col>
-        <v-col>
+        <v-col cols="12" sm="4" class="my-2 px-1">
           <h3>Congestion Chart</h3>
-          <congestion-chart :items="users"/>
+          <congestion-chart :items="result"/>
         </v-col>
-        <v-col>
+        <v-col cols="12" sm="4" class="my-2 px-1">
           <h3>Danger Chart</h3>
-          <danger-chart :items="users"/>
+          <danger-chart :items="result"/>
         </v-col>
       </v-row>
     </v-container>
@@ -42,6 +23,7 @@
 <script>
   import CongestionChart from '@/components/CongestionChart'
   import DangerChart from '@/components/DangerChart'
+  import axios from 'axios'
 
   export default {
     name: 'UI_02',
@@ -51,27 +33,31 @@
     },
     data() {
       return {
-        users: []
+        result: '',
+        picker: new Date().toISOString().substr(0, 10),
+        date: '',
       }
     },
     created() {
       this.getData() //anytime the vue instance is created, call the fetchData() function.
     },
     methods: {
+      formatDate(date) {
+            if (!date) return null
+            const [year, month, day] = date.split('-')
+            return `${year}/${month}/${day}`
+      },
       getData() {
-        this.$http
-          .get('https://115.145.212.100:53344/api/stats')
-          .then(response => {
-            return response.json();
+        this.date = this.formatDate(this.picker)
+        axios.get('https://115.145.212.100:53344/api/stats'+ this.date)
+          .then(res => {
+            this.result = res.data
+            console.log(res.data)
           })
-          .then(data => {
-            this.users = data;
-         });
-      }
+      },
     }
   }
 </script>
-
 
 <style>
 </style>
